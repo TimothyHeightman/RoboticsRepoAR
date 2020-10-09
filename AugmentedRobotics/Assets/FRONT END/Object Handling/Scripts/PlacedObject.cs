9f97ef6471a07991f4760dd72d474e58594a28aa
Abdullah Rehman <ayr18@ic.ac.uk>
2020-10-09 15:58:47 +0100

    AR Preliminary Push

---
+++ AugmentedRobotics/Assets/FRONT END/Object Handling/Scripts/PlacedObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacedObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private static GameObject objectToPlace;
+
+    [SerializeField]
+    private static GameObject placementIndicator;
+
+    private bool IsSelected;
+
+    public bool Selected {
+        get {
+            return this.IsSelected;
+        }
+        set {
+            IsSelected = value;
+        }
+    }
+
+}
