170118f0d2f78a3b25f0c08bfe0fc46bc0e9201d
Luke Murray <lm318@ic.ac.uk>
2020-11-09 12:27:52 +0000

    Misc Fixes

--- AugmentedRobotics/Assets/BACKEND/Skeleton/StandaloneSkeleton.cs
+++ AugmentedRobotics/Assets/BACKEND/Skeleton/StandaloneSkeleton.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TreeEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class StandaloneSkeleton : MonoBehaviour
