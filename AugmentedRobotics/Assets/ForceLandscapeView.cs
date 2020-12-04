54053b21d0eb7f0e4cd084b15a28f05ea4c196bf
Patrick <pmassowalsh@gmail.com>
2020-12-04 15:05:45 +0000

    Missing Script & Material Stuff

---
+++ AugmentedRobotics/Assets/ForceLandscapeView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ForceLandscapeView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+}
